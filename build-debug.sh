cc019fb7f1dd5913250e2aa6f1e67d5bde19ce0d
jpitkanen18 <jpitkanen18@gmail.com>
2025-04-07 21:38:19 +0300

    New and improved toolchain

--- build-debug.sh
+++
@@ -1,3 +0,0 @@
-# !/bin/zsh
-clang++ -g -std=c++17 src/*.cpp -o jim-debug -lncurses -DDEBUG
- 
---
+++ build.zsh
@@ -0,0 +1,5 @@
+# !/bin/zsh
+echo "✨ Building Jim... The Only Text Editor That Fights Back ✨"
+echo "🧨 If it crashes, it’s a feature. 🧨"
+clang++ -g -std=c++17 src/*.cpp -o jim -lncurses
+ 
